624f5cf69cc41d52bb972afbd6682cae25a5fc11
emmanuel <crackseba201@gmail.com>
2023-12-11 10:41:25 -0300

    vinculé el proyecto con la base de datos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { SharedModule } from './shared/shared.module';
 
+//firebase
+import { environment } from 'src/environments/environment';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+
 //componentes
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    //firebase
+    AngularFireModule.initializeApp(environment.firebaseConfig), //inicializa firebase
+    AngularFireAuthModule,
+    AngularFireStorageModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production : false,
+    
+     firebaseConfig : {
+        apiKey: "AIzaSyBQLwIFyKu2mZYL07iQG92Si2ePYA-cx98",
+        authDomain: "metal-app-ba463.firebaseapp.com",
+        projectId: "metal-app-ba463",
+        storageBucket: "metal-app-ba463.appspot.com",
+        messagingSenderId: "668663384503",
+        appId: "1:668663384503:web:13e7e47c92fc3155374032",
+        measurementId: "G-732EVZW52P"
+      }  //se inicializa la base de datos en el proyecto
+}
