48f8ec63d75ff74aab7009ec7694383f6e1aeebd
emmanuel <crackseba201@gmail.com>
2023-12-11 11:38:08 -0300

    configuración de firebase terminada

--- src/environments/environment.prot.ts
+++ src/environments/environment.prot.ts
@@ -1,3 +1,13 @@
 export const environment = {
-    production : true
+    production : true,
+
+    firebaseConfig : {
+        apiKey: "AIzaSyBQLwIFyKu2mZYL07iQG92Si2ePYA-cx98",
+        authDomain: "metal-app-ba463.firebaseapp.com",
+        projectId: "metal-app-ba463",
+        storageBucket: "metal-app-ba463.appspot.com",
+        messagingSenderId: "668663384503",
+        appId: "1:668663384503:web:13e7e47c92fc3155374032",
+        measurementId: "G-732EVZW52P"
+      }
 }
